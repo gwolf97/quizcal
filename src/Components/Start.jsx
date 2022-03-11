487f9ece45334ca2c391c4b10c55b5499c14acec
Gabriel M. Wolf <92558787+gwolf97@users.noreply.github.com>
2022-03-11 16:07:51 -0500

    Start page component added

---
+++ src/Components/Start.jsx
@@ -0,0 +1,16 @@
+const Start = () => {
+    return ( 
+        <>
+          <div className="yellow"></div>
+        <div className="start-container">
+          
+            <h1>Quizical</h1>
+            <p>Think you know everything? Quizical will put you to the test!</p>
+            <button>Start quiz</button>
+        </div>
+        <div className="blue"></div>
+        </>
+     );
+}
+ 
+export default Start;
