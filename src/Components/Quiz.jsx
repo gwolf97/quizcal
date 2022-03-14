12bdfc738c9a6897ca38161a1307b5f159cccae0
Gabriel M. Wolf <92558787+gwolf97@users.noreply.github.com>
2022-03-14 15:09:08 -0500

    Special characters decoded

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { decode } from "html-entities"
 import Question from "./Question"
 
 const Quiz = (props) => {
@@ -10,7 +11,8 @@ const Quiz = (props) => {
     React.useEffect(() => {
         fetch(`https://opentdb.com/api.php?amount=5&type=multiple`)
             .then(res => res.json())
-            .then(data => setQuestionData({
+            .then(data =>
+                setQuestionData({
                 questions: data.results,
                 dataLoaded: true
             }))
@@ -23,7 +25,7 @@ const Quiz = (props) => {
     return ( 
         <div className="quiz-container">
             <ul>
-                {questionData.dataLoaded && questionData.questions.map(object => (<li>{object.question}</li>))}
+                {questionData.dataLoaded && questionData.questions.map(object => (<li>{decode(object.question)}</li>))}
             </ul>
         </div>
      );
