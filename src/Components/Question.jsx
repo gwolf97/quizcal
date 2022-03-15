579d5695ffa478c457e9668beab77197df64edbf
Gabriel M. Wolf <92558787+gwolf97@users.noreply.github.com>
2022-03-15 18:05:31 -0500

    Verifying if answer is correct increasing counter if so

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -3,10 +3,30 @@ import { decode } from "html-entities";
 
 const Question = (props) => {
 
+
+    
+    
+
 const incorrect = props.incorrect
 const correct = props.correct
-const allAnswers = [...incorrect, correct]
-const newAnswers = allAnswers.sort((a,b) => 0.5 - Math.random());
+let allAnswers = [...incorrect, correct]
+let newAnswers = allAnswers
+
+useEffect(()=>{
+    function shuffle(){
+         allAnswers.sort((a,b) => 0.5 - Math.random());
+    }
+    shuffle()
+}, [])
+
+//useEffect(() =>{
+  //  let answer = selectedAnswers.slice(-1)[0] 
+    //console.log(answer)
+//},[selectedAnswers])
+
+
+
+
 
 
 //<button onClick={props.answerChosen} value={newAnswers[0]}>{decode(newAnswers[0])}</button>
@@ -19,20 +39,20 @@ const newAnswers = allAnswers.sort((a,b) => 0.5 - Math.random());
             <li className="question">{decode(props.question)}</li>
             <ul className="answer-buttons">
             <li>
-    <input type="radio" id={newAnswers[0]} name={props.question} />
-    <label htmlFor={newAnswers[0]}>{newAnswers[0]}</label>
+    <input onChange={props.handleChange} type="radio" id={newAnswers[0]} name={props.question} />
+    <label htmlFor={newAnswers[0]}>{decode(newAnswers[0])}</label>
   </li>
   <li>
-    <input type="radio" id={newAnswers[1]} name={props.question} />
-    <label htmlFor={newAnswers[1]}>{newAnswers[1]}</label>
+    <input onChange={props.handleChange} type="radio" id={newAnswers[1]} name={props.question} />
+    <label htmlFor={newAnswers[1]}>{decode(newAnswers[1])}</label>
   </li>
   <li>
-    <input type="radio" id={newAnswers[2]} name={props.question} />
-    <label htmlFor={newAnswers[2]}>{newAnswers[2]}</label>
+    <input onChange={props.handleChange} type="radio" id={newAnswers[2]} name={props.question} />
+    <label htmlFor={newAnswers[2]}>{decode(newAnswers[2])}</label>
   </li>
   <li>
-    <input type="radio" id={newAnswers[3]} name={props.question} />
-    <label htmlFor={newAnswers[3]}>{newAnswers[3]}</label>
+    <input onChange={props.handleChange} type="radio" id={newAnswers[3]} name={props.question} />
+    <label htmlFor={newAnswers[3]}>{decode(newAnswers[3])}</label>
   </li>
             </ul>
         </div>
--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -8,11 +8,10 @@ const Quiz = (props) => {
         dataLoaded: false
     })
 
-    const [selectedAnswers, setSelectedAnswers] = React.useState({
-        
-    })
-
-
+    const [amountCorrect,setAmountCorrect] = React.useState(0)
+    const [selectedAnswers, setSelectedAnswers] = React.useState([])
+    const [finalAnswers, setFinalAnswers] = React.useState([])
+    const [correctAnswers, setCorrectAnswers] = React.useState([])
 
     React.useEffect(() => {
         fetch(`https://opentdb.com/api.php?amount=5&type=multiple`)
@@ -24,16 +23,35 @@ const Quiz = (props) => {
             }))
     }, [props.start])
 
+    function handleChange(e){
+        const {name, id} = e.target;
+        setSelectedAnswers(prev => {
+            for (let i = 0; i < prev.length; i++){
+            if (prev[i].question === name){
+                prev.splice(i,1)
+            }
+        } return[...prev, {question:name,answer:id}]})}
+
+    function handleClick(){
+        questionData.questions.map(obj => setCorrectAnswers(prev => [...prev,obj.correct_answer]))
+        selectedAnswers.map(obj => setFinalAnswers(prev => [...prev, obj.answer]))
+        
+        finalAnswers.map(answer =>{
+            correctAnswers.includes(answer) && setAmountCorrect(prevAmount => prevAmount + 1)
+        } )
+    }
     console.log(questionData.questions)
 
+console.log()
     return ( 
         <>
         <div className="small-yellow"></div>
             <div className="quiz-container">
                 <ul>
-                    {questionData.dataLoaded && questionData.questions.map(object => (<Question question={object.question} incorrect={object.incorrect_answers} correct={object.correct_answer}/>))}
+                    {questionData.dataLoaded && questionData.questions.map(object => (<Question question={object.question} incorrect={object.incorrect_answers} correct={object.correct_answer} handleChange={handleChange}/>))}
                 </ul>
-                {questionData.dataLoaded && <button className="check-answers">Check answers</button>}
+                {questionData.dataLoaded && <button className="check-answers" onClick={handleClick}>Check answers</button>}
+                    <div>{amountCorrect}/5</div>
             </div>
         <div className="small-blue"></div>
         </>
